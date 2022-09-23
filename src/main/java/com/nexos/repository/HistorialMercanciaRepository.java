4c8bec1f17ee7eec6405cbdb2daa54cb72f2010a
Estefany Rodriguez Bedoya <erodriguez1193@gmail.com>
2022-09-23 17:14:55 -0500

    Query

--- src/main/java/com/nexos/repository/HistorialMercanciaRepository.java
+++ src/main/java/com/nexos/repository/HistorialMercanciaRepository.java
@@ -11,7 +11,7 @@ import com.nexos.model.HistorialMercancia;
 @Repository
 public interface HistorialMercanciaRepository extends JpaRepository<HistorialMercancia, Long> {
 	
-	@Query("SELECT h.id, e.nombre, m.nombre, h.fecha_modificacion, h.operacion\r\n"
+	@Query("SELECT h.id, m.nombre, e.nombre, h.fecha_modificacion, h.operacion\r\n"
 			+ "FROM HistorialMercancia h\r\n"
 			+ "JOIN Mercancia m\r\n"
 			+ "ON h.id_mercancia = m.id"
