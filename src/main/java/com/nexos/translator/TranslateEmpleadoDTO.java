e3c67ca383082cf21ceb2acae3fa7fbf92f4742c
Estefany Rodriguez Bedoya <erodriguez1193@gmail.com>
2022-10-21 14:35:45 -0500

    input

--- src/main/java/com/nexos/translator/TranslateEmpleadoDTO.java
+++ src/main/java/com/nexos/translator/TranslateEmpleadoDTO.java
@@ -8,16 +8,16 @@ import com.nexos.model.Empleado;
 @Component
 public class TranslateEmpleadoDTO implements Translator<Empleado, EmpleadoDTO> {
 
-	@Override
-	public EmpleadoDTO translate(Empleado empleado) {
-		
-		EmpleadoDTO imput = new EmpleadoDTO();
-		imput.setId(empleado.getId());
-		imput.setNombre(empleado.getNombre());
-		imput.setApellido(empleado.getApellido());
-		imput.setEdad(empleado.getEdad());
-		imput.setId_cargo(empleado.getId_cargo());
-		imput.setFecha_ingreso_empresa(empleado.getFecha_ingreso_empresa());
-		return imput;
-	}
+  @Override
+  public EmpleadoDTO translate(Empleado empleado) {
+
+    EmpleadoDTO input = new EmpleadoDTO();
+    input.setId(empleado.getId());
+    input.setNombre(empleado.getNombre());
+    input.setApellido(empleado.getApellido());
+    input.setEdad(empleado.getEdad());
+    input.setId_cargo(empleado.getId_cargo());
+    input.setFecha_ingreso_empresa(empleado.getFecha_ingreso_empresa());
+    return input;
+  }
 }
