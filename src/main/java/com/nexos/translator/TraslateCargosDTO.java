b08d70bac5a18461155d0c26f03eda67841702fc
Estefany Rodriguez Bedoya <erodriguez1193@gmail.com>
2022-10-26 11:39:26 -0500

    @Component

--- src/main/java/com/nexos/translator/TraslateCargosDTO.java
+++ src/main/java/com/nexos/translator/TraslateCargosDTO.java
@@ -1,8 +1,11 @@
 package com.nexos.translator;
 
+import org.springframework.stereotype.Component;
+
 import com.nexos.domain.CargosDTO;
 import com.nexos.model.Cargos;
 
+@Component
 public class TraslateCargosDTO implements Translator<Cargos, CargosDTO> {
 
 	@Override
