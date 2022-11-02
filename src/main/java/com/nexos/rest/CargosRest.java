c1ac13b8a1b7ed2e86cf129a195c6bbc1d3c0313
Estefany Rodriguez Bedoya <erodriguez1193@gmail.com>
2022-11-02 17:03:24 -0500

    Eliminación del Cross

--- src/main/java/com/nexos/rest/CargosRest.java
+++ src/main/java/com/nexos/rest/CargosRest.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +18,6 @@ import com.nexos.domain.CargosDTO;
 import com.nexos.model.Cargos;
 import com.nexos.service.CargosService;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping ("/Cargos")
 public class CargosRest {
--- src/main/java/com/nexos/rest/MercanciaRest.java
+++ src/main/java/com/nexos/rest/MercanciaRest.java
@@ -8,7 +8,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +21,6 @@ import com.nexos.domain.MercanciaDTO;
 import com.nexos.model.Mercancia;
 import com.nexos.service.MercanciaService;
 
-@CrossOrigin(origins = {"http://localhost:4200"})
 @RestController
 @RequestMapping("/Mercancia/")
 public class MercanciaRest {
